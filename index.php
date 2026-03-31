21b27e89c65d1613fb57363f9babd9be061dc20c
katherin ross <wvalersanchez@gmail.com>
2026-03-31 10:23:44 -0500

    Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Gourmet Criollo</title>
+</head>
+<body>
+    <div class="layout"> 
+        <header class="header">
+            <div class="header_contenido container">
+                <p class="header_logo">Gourmet Criollo</p>
+                
+                <nav class="header_nav">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Carta</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </nav>
+
+                <a class="cta-primary" href="#">Reserva Mesa</a>
+            </div>
+        </header>
+
+        <main>
+            <p>Contenido Principal</p>
+        </main>  
+
+        <footer class="footer">
+            <div class="container">
+                <p>footer &copy; 2026</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
